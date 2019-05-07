711c99e71f879992f7641d6b2f0eba6b67de20e6
tempwork <tempwork@foxmail.com>
2019-05-07 11:27:04 +0800

    @itenl 动态正则匹配

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
 module.exports = {
   // 是否随机文件名
-  isRandomFileName: false,
+  randomName: false,
   // 输出路径
   distPath: './dist',
+  // 自动子路径
+  // depth > 0
+  autoChildPath: true,
+  // 允许下载文件后缀
+  mime: ['jpg', 'gif', 'png'],
+  // 正则匹配内容时忽略 http:或https:协议
+  ignoreProtocol: true,
   // 文件名过滤规则
   filter: fileName => {
     return fileName.replace(/(http|https):\/\/|\/|\?/g, '-');
