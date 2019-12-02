3d6c7640ac4289ad0fe4221de2a1733bddac2bc4
itenl <nesa.wong@gmail.com>
2019-12-02 15:32:02 +0800

    @itenl 配置 http proxy，添加自定义 referer 头

--- src/config.js
+++ src/config.js
@@ -9,11 +9,12 @@ module.exports = {
   // 是否保存远程响应的内容（HTML/API-JSON等）
   autoSaveRemoteContent: false,
   // 允许下载文件后缀
-  mime: ['jpg', 'gif', 'png'],
-  // socks5 Proxy
-  shttp: {
-    socksHost: '127.0.0.1',
-    socksHost: '1086'
+  mime: ['jpg', 'jpeg', 'gif', 'png'],
+  // http Proxy
+  proxy: {
+    hostname: '127.0.0.1',
+    port: '1087',
+    enable: false
   },
   // 正则匹配内容时忽略 http:或https:协议 已强制忽略
   // ignoreProtocol: true,
